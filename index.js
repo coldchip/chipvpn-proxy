2a99aa90af8b20108ce733104687dac06d11f202
Ryan <growwick@gmail.com>
2022-03-25 23:57:56 +0800

    update proxy

--- index.js
+++ index.js
@@ -1,53 +1,95 @@
 const express = require('express');
 const net = require('net');
 
-const app = express()
-const port = 80
+var sessions = [];
+
+const app = express();
+const port = 80;
 
 app.get('/', (req, res) => {
- 	res.send('ChipVPN proxy')
-})
+	res.set("Content-Type", "application/json");
+
+	var filtered = [];
+	for(const session of sessions) {
+		var s = session;
+		s.ip = "[REDACTED]";
+		filtered.push(s);
+	}
+
+	res.send(filtered);
+});
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
 })
 
-var server = net.createServer();
+var server = net.createServer((client) => {
+	console.log("Client connect", client.remoteAddress, client.remotePort);
 
-server.on("connection", (client) => {
-	console.log("Client connect");
-	client.setTimeout(1000);
+	sessions.push({
+		ip: client.remoteAddress,
+		port: client.remotePort,
+		tx: 0,
+		rx: 0
+	});
 
 	var remote = new net.Socket();
-	remote.connect(443, '127.0.0.1', () => {
-		console.log('Remote Connected');
-		remote.setTimeout(1000);
+	remote.connect(443, 'vpn.coldchip.ru');
 
-		client.on('data', (data) => {
-			remote.write(data);
-		});
-		client.on('end', function () {
-			console.log('Client disconnect.');
-			remote.end();
-		});
-		client.on('timeout', function () {
-			remote.end();
-		});
+	client.on('data', (data) => {
+		for(const session of sessions) {
+			if(session.ip === client.remoteAddress && session.port === client.remotePort) {
+				console.log("client -> remote", data);
 
-		remote.on('data', (data) => {
-			client.write(data);
-		});
-		remote.on('end', function () {
-			console.log('Client disconnect.');
-			client.end();
-		});
-		remote.on('timeout', function () {
-			client.end();
-		});
+				session.tx += data.length;
+
+				var w = remote.write(data);
+				if(!w) {
+					client.pause();
+				}
+			}
+		}
+	});
+	remote.on('data', (data) => {
+		for(const session of sessions) {
+			if(session.ip === client.remoteAddress && session.port === client.remotePort) {
+				console.log("remote -> client", data);
+				session.rx += data.length;
+				
+				var w = client.write(data);
+				if(!w) {
+					remote.pause();
+				}
+			}
+		}
+	});
+
+	client.on('drain', (data) => {
+		remote.resume();
+	});
+	remote.on('drain', (data) => {
+		client.resume();
+	});
+
+	client.on('error', function() {
+		remote.end();
+	});
+	remote.on('error', function() {
+		client.end();
+	});
+
+	client.on('close', function () {
+		console.log('Client disconnect.');
+		sessions = sessions.filter(session => session.ip !== client.remoteAddress && session.port !== client.remotePort)
+		remote.end();
+	});
+	remote.on('close', function () {
+		console.log('Client disconnect.');
+		client.end();
 	});
 });
 
-server.listen(8443, function () {
+server.listen(443, function () {
 	// Get server address info.
 	var serverInfo = server.address();
 	var serverInfoJson = JSON.stringify(serverInfo);
